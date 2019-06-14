60b3e97e1ac1eb97b61d54e70f43950c4e6b7416
Varun Vachhar <varunvachhar@gmail.com>
2019-06-14 16:48:15 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -4,30 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-loadPolyfills().then(() => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-});
-
-/**
- * Do feature detection, to figure out which polyfills needs to be imported.
- **/
-function loadPolyfills() {
-  const polyfills = [];
-
-  if (!supportsResizeObserver()) {
-    polyfills.push(import('resize-observer-polyfill'));
-  }
-
-  return Promise.all(polyfills);
-}
-
-function supportsResizeObserver() {
-  return (
-    'ResizeObserver' in global &&
-    'ResizeObserverEntry' in global &&
-    'contentRect' in ResizeObserverEntry.prototype
-  );
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
